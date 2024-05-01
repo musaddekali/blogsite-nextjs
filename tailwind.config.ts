e7f7901e943beb1cccf67211033ddcbe1aed3e18
MusaddekAli <musaddekali.dev@gmail.com>
2024-05-01 15:57:51 +0600

    basic setup done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,11 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    container: {
+      center: true,
+      padding: "0.75rem",
     },
+    extend: {},
   },
   plugins: [],
 };
