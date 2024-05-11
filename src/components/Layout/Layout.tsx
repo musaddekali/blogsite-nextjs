c19d56e2165fa3f9ad72fc7fd2937a427350813c
MusaddekAli <musaddekali.dev@gmail.com>
2024-05-11 18:26:17 +0600

    removed comment from layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-//hello from main
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
